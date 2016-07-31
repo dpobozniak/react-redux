164bcd5580f55d498fc61f3ab246c46e68d4b95b
Dariusz Pobożniak <dariusz.pobozniak@performgroup.com>
2016-07-31 21:08:23 +0200

    404 route

---
+++ src/NotFound.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const NotFound = () => (
+  <h2>404... This page is not found!</h2>
+);
+
+export default NotFound;
--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 
 import AppComponent from './App';
+import NotFoundComponent from './NotFound';
 import IndexComponent from './IndexComponent';
 import AboutComponent from './AboutComponent';
 
@@ -9,5 +10,6 @@ export default (
   <Route path="/" component={AppComponent}>
     <IndexRoute component={IndexComponent} />
     <Route path="about" component={AboutComponent} />
+    <Route path='*' component={NotFoundComponent} />
   </Route>
 );
